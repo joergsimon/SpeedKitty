bb8ce0859839ff1cd20ad199b7d1dd4cb88b463c
Joerg Simon <j_simon@mac.com>
2015-04-21 19:39:31 +0200

    update DB to allow to store a history of runs

--- app/src/main/java/android/upipc/knowcenter/at/speedkitty/db/DatabaseHandler.java
+++ app/src/main/java/android/upipc/knowcenter/at/speedkitty/db/DatabaseHandler.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,7 +17,7 @@ import java.util.List;
  */
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME = "SpeedKittyDB";
 
     private static final String KEY_ID = "_id"; // global id key
@@ -28,6 +29,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String KEY_RUN_START = "start_timestamp";
     private static final String KEY_RUN_END = "end_timestamp";
     private static final String KEY_RUN_AVARAGE_SPEED = "speed";
+    private static final String[] ALL_RUN_FIELDS = new String[] { KEY_ID,
+            KEY_RUN_START, KEY_RUN_END, KEY_RUN_AVARAGE_SPEED };
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -70,8 +73,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         values.put(KEY_COUNT, speed);
 
         // updating row
-        db.update(TABLE_SPEED, values, KEY_ID + " = ?",
-                new String[]{"1"});
+        db.update(TABLE_SPEED, values, KEY_ID + " = ?", idOne());
         db.close();
     }
 
@@ -79,15 +81,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = db.query(TABLE_SPEED, new String[]{KEY_COUNT}, KEY_ID + "=?",
-                new String[]{"1"}, null, null, null, null);
+                idOne(), null, null, null, null);
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
         } else {
             insertSpeed(1);
             return 1;
         }
-        int results = cursor.getCount();
-        int position = cursor.getPosition();
         String speedStr = cursor.getString(0);
         db.close();
         return Integer.parseInt(speedStr);
@@ -110,7 +110,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         // Inserting Row
         db.update(TABLE_SPEED, values, KEY_ID + " = ?",
-                new String[]{run.getId()});
+                wrap(run.id));
         db.close(); // Closing database connection
     }
 
@@ -123,10 +123,73 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
     public List<Run> getAllRuns() {
-        return null; // TODO: implement
+        List<Run> runsList = new ArrayList<Run>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_RUNS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Run run = fromCursor(cursor);
+                runsList.add(run);
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return runsList;
+    }
+
+    public Run getRun(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_RUNS, ALL_RUN_FIELDS, KEY_ID + "=?",
+                wrap(id), null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Run run = fromCursor(cursor);
+        return run;
     }
 
     public Run getLastRun() {
-        return null; // TODO: implement
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_RUNS, ALL_RUN_FIELDS, KEY_RUN_END+"=(SELECT MAX("+KEY_RUN_END+"))",
+                null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        if (cursor.getCount() == 0) {
+            return null;
+        }
+
+        Run run = fromCursor(cursor);
+        return run;
+    }
+
+    private Run fromCursor(Cursor cursor) {
+        int id = Integer.parseInt(cursor.getString(0));
+        long start = Long.parseLong(cursor.getString(1));
+        long end = Long.parseLong(cursor.getString(2));
+        float speed = Float.parseFloat(cursor.getString(3));
+        Run run = new Run(id, start, end, speed);
+        return run;
+    }
+
+    public void deleteRun(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_RUNS, KEY_ID + " = ?", wrap(id));
+        db.close();
+    }
+
+    private String[] wrap(int num) {
+        return new String[] { String.valueOf(num)};
+    }
+
+    private String[] idOne() {
+        return new String[]{"1"};
     }
 }
--- app/src/main/java/android/upipc/knowcenter/at/speedkitty/db/Run.java
+++ app/src/main/java/android/upipc/knowcenter/at/speedkitty/db/Run.java
@@ -10,18 +10,32 @@ public class Run {
     protected float avarageSpeed;
 
     /**
-     *
      * @param start should be a timestamp IN SECONDS
      * @param end should be a timestamp IN SECONDS
      * @param avarageSpeed float for km/h speed
      */
     public Run(long start, long end, float avarageSpeed) {
+        this(-1, start, end, avarageSpeed);
+    }
+
+    /**
+     * @param id id of the record in the database
+     * @param start should be a timestamp IN SECONDS
+     * @param end should be a timestamp IN SECONDS
+     * @param avarageSpeed float for km/h speed
+     */
+    public Run(int id, long start, long end, float avarageSpeed) {
+        this.id = id;
         this.start = start;
         this.end = end;
         this.avarageSpeed = avarageSpeed;
     }
 
-    public String getId() {
-        return ""+id;
+    public long getStart() {
+        return start;
+    }
+
+    public long getEnd() {
+        return end;
     }
 }
